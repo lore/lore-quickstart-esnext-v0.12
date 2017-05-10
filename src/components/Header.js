ad23e145bf80e2857418f819c28d39a1311f5404
Jason Hansen <jason_hansen@outlook.com>
2017-05-09 23:22:25 -0700

    [layout] step 3, add header to layout

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React, { Component, PropTypes } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <nav className="navbar navbar-default navbar-static-top header">
+        <div className="container">
+          <div className="navbar-header">
+            <div className="navbar-brand">
+              Lore Quickstart
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+
+}
+
+export default Header;
