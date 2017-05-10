40e5bc7349d57f1e5d4d2b08963ed3777129fe8f
Jason Hansen <jason_hansen@outlook.com>
2017-05-09 23:45:15 -0700

    [data] step 3, add mock user data

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,11 +4,24 @@ import moment from 'moment';
 class Tweet extends Component {
 
   static propTypes = {
-    tweet: PropTypes.object.isRequired
+    tweet: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired
+  };
+
+  static defaultProps = {
+    user: {
+      id: 1,
+      data: {
+        id: 1,
+        nickname: "lucca",
+        avatar: "https://cloud.githubusercontent.com/assets/2637399/19027072/a36f0c7a-88e1-11e6-931e-7f67fe01367b.png"
+      }
+    }
   };
 
   render() {
     const tweet = this.props.tweet;
+    const user = this.props.user;
     const timestamp = moment(tweet.data.createdAt).fromNow().split(' ago')[0];
 
     return (
@@ -16,11 +29,11 @@ class Tweet extends Component {
         <div className="image-container">
           <img
             className="img-circle avatar"
-            src={'http://ssl.gstatic.com/images/icons/material/product/1x/avatar_circle_blue_120dp.png'} />
+            src={user.data.avatar} />
         </div>
         <div className="content-container">
           <h4 className="list-group-item-heading title">
-            Nickname
+            {user.data.nickname}
           </h4>
           <h4 className="list-group-item-heading timestamp">
             {'- ' + timestamp}
